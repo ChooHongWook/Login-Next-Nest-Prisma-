7b2b96d3dc786c158b0a4922524402f9befdfc00
Hong Wook Choo <hw.choo@amicuslex.net>
2025-07-27 11:01:26 +0900

    Feat(backend): update DATABASE_URL and add dotenv and dotenv-expand for better env usability

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
+
+const myEnv = dotenv.config();
+dotenvExpand.expand(myEnv);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
