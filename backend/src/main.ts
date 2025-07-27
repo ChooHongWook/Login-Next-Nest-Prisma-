2cccef740a65d7319025f43d287a4d468dcf4eec
Hong Wook Choo <hw.choo@amicuslex.net>
2025-07-27 19:33:47 +0900

    Feat(swagger): add Swagger documentation setup for API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const myEnv = dotenv.config();
 dotenvExpand.expand(myEnv);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Inflearn API 문서')
+    .setDescription('Inflearn API 문서입니다.')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'access-token',
+        description: 'Enter access token',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 8888);
 }
 bootstrap();
