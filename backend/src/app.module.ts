ee95b66b7c48744160aa1dec8c03e3791cea4fad
Hong Wook Choo <hw.choo@amicuslex.net>
2025-07-27 10:40:51 +0900

    Feat(backend): configure environment variables and add ConfigModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전체 모듈에서 사용 가능하도록 적용
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
