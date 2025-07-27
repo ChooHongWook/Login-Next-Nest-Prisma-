5d47f142ee6b9aa96209a7be203f462cdad13a87
Hong Wook Choo <hw.choo@amicuslex.net>
2025-07-27 19:04:44 +0900

    Feat(auth): add signup functionality and user authentication actions

---
+++ frontend/app/(path)/signin/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+
+export default function SigninPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    signIn("credentials", {
+      email,
+      password,
+      redirectTo: "/",
+    });
+  };
+
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-3xl font-bold">로그인</h1>
+      <p className="text-gray-700">로그인 페이지 입니다</p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-2 min-w-[300px]"
+      >
+        <label htmlFor="email">이메일</label>
+        <input
+          value={email}
+          onChange={handleChangeEmail}
+          id="credentials-email"
+          type="email"
+          name="email"
+          placeholder="이메일을 입력해 주세요"
+          className="border-2 border-gray-300 rounded-sm p-2"
+        />
+        <label htmlFor="password">비밀번호</label>
+        <input
+          value={password}
+          onChange={handleChangePassword}
+          id="credentials-password"
+          type="password"
+          name="password"
+          placeholder="비밀번호를 입력해 주세요"
+          className="border-2 border-gray-300 rounded-sm p-2"
+        />
+
+        <button
+          type="submit"
+          className="bg-primary text-white font-bold cursor-pointer rounded-sm p-2"
+        >
+          로그인
+        </button>
+        <Link href="/signup" className="text-center">
+          회원가입
+        </Link>
+      </form>
+    </div>
+  );
+}
